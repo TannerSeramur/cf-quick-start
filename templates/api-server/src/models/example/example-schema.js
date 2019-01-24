82de15cdf3827eedb9b310645b62a788bcf544c6
BECCA LEE <REBECCA.KIRSTIN.LEE@GMAIL.COM>
2019-01-24 13:55:27 -0800

    Hotfix for api testing issues

--- templates/api-server/src/models/example/example-schema.js
+++ templates/api-server/src/models/example/example-schema.js
@@ -5,8 +5,7 @@ require('mongoose-schema-jsonschema')(mongoose);
 
 const example = mongoose.Schema({
   name: { type:String, required:true },
-  trueorfalse: { type:String, required:true, uppercase:true, enum:['T', 'F'] },
-  group: {type:String, required:true},
-});
+  description: { type:String, required:true },
+}, { toObject:{virtuals:true}, toJSON:{virtuals:true} });
 
 module.exports = mongoose.model('example', example);
